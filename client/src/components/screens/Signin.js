edf9d10fb2ebf6473db0ba2b6cf1a7b06a080e3e
prafful-github <135363758+prafful-github@users.noreply.github.com>
2024-02-18 15:19:04 +0530

    Update Signin.js

--- client/src/components/screens/Signin.js
+++ client/src/components/screens/Signin.js
@@ -15,7 +15,7 @@ const Signin = ()=>{
         if(!/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email)){
             return M.toast({html:"Invalid email", classes:"#f44336 red"})
         }
-        fetch("http://localhost:5000/signin",{
+        fetch("/signin",{
             method:"post",
             headers:{
                 "Content-Type":"application/json"
